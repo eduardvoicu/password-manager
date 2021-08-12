69232ff8ee642669833df0e015e18055f2a17377
Eduard Voicu <iameduardvoicu@gmail.com>
2021-08-12 07:32:45 -0500

    Added more const and if else statements to JS script.

--- develop/script.js
+++ develop/script.js
@@ -8,21 +8,30 @@ var passwordLength;
 var uppercaseCheck;
 var numberCheck;
 var specialCheck;
+var userChoice;
 
 // Phase 1: Determine the length of password from minimum 8 characters to 128 maximum characters
 // Next few functions are to catch any errors made by the user inputing anything less than 8, more than 128 and/or
 // a word instead of a number.
 
+const uppercaseCheckOk = true;
+const uppercaseCheckCancel = false;
+const numberCheckOk = true;
+const numberCheckCancel = false;
+const specialCheckOk = true;
+const specialCheckCancel = false;
+
+
+
 function determineLength(){
 
-  if (passwordLength<8){
+  
+  if (passwordLength<8 || passwordLength>128){
       alert("Oops! Something went wrong. Please enter a number between 8 and 128.");
       determineLength();
   }
-  else if (passwordLength>128){
-      alert("Oops! Something went wrong. Please enter a number between 8 and 128.");
-      determineLength();
-  }
+  // Try passwordlength>128 separate.
+
   else if (isNaN(passwordLength)){
     alert("Oops! Something went wrong. Please enter a number between 8 and 128.");
     determineLength();
@@ -31,7 +40,9 @@ function determineLength(){
     alert("Please choose your options on what you want your random password to include on the next few prompts.");
   }
   return passwordLength;
+  
 }
+
 var userChoice = window.prompt("Enter length of password between no less than 8 characters and no more than 128, please.");
 
 //If user enters >8, proceed to next question in regards to lower case or uppercase.
